adfeb0dec90de64fb282104c88d2f8329b824820
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-03-20 17:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const student = {
+	name: "Abcd",
+}
+
+Object.prototype.getKeys= function() {
+	let array=[];
+
+    let ar= Object.entries(this);
+
+     for(let i=0;i<ar.length;i++) {
+         let key=ar[i][0];
+
+         array.push(key);
+     }
+
+    return array;
+}
+
+student.getKeys();
